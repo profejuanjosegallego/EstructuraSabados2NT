2b18fc3a9966b129d290c136af88a54bb8234a13
Alejandro <vvasco@cesde.net>
2026-02-14 11:49:03 -0500

    feat se crea diccionario estudiante

---
+++ diccionario.py
@@ -0,0 +1,12 @@
+#Creando un diccionario
+estudiante={}
+
+#Poblando un diccionario
+estudiante["id"]=12345
+estudiante["documento"]="1234567890"
+estudiante["telefono"]="5551234"
+estudiante["promedio"]=3.5  
+estudiante["correo"]="correo@gmail.com"
+estudiante["semestre"]=3
+estudiante["nombre"]="Juan Perez"
+estudiante["esBecado"]=True
