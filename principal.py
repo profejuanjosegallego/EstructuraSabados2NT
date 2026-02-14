b3d1731f5450ec39fe211551611e78b167660fc1
d4anni.e <dannie.sft@gmail.com>
2026-02-14 11:48:54 -0500

    feat se crea diccionario estudiante

---
+++ principal.py
@@ -0,0 +1,8 @@
+idEstudiante=3636363
+documentoEstudiante="233232"
+telefono="3225962363"
+promedio=3.5
+correoEstudiante="correo@gmail.com"
+semestreEstudiante=3
+nombreEstudiante= "Juan Gallego"
+esBecado=True
