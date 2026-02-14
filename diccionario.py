3b069e703cabfec9aeda9fadd8ba7546e5c2f804
Alejo <alejozapata432.com>
2026-02-14 13:59:27 -0500

    feat se crea diccionario bicicleta1 y bicicleta2

--- diccionario.py
+++ diccionario.py
@@ -1,7 +1,5 @@
 #Creando un diccionario
-estudiante = {
-    id: 32
-}
+estudiante = {}
 
 #poblando el diccionario
 estudiante ["id"] = 32
@@ -13,5 +11,30 @@ estudiante ["semestre"] = 3
 estudiante ["nombre"] = "Juan Gallego"
 estudiante ["esBecado"] = True
 
+##crear diccionario de bicicleta1
+bicicleta = {}
+
+##poblando el diccionario de bicicleta1
+bicicleta ["id"] = 1
+bicicleta ["material"] = "plastilina"
+bicicleta ["ancho"] = 1.5
+bicicleta ["largo"] = 2.0
+bicicleta ["alto"] = 1.0
+bicicleta ["esFuncional"] = False
+bicicleta ["esEstable"] = False
+bicicleta ["esParecida"] = True
 
 
+##crear diccionario de bicicleta2
+bicicleta2 = {}
+
+
+##poblando el diccionario de bicicleta 2
+bicicleta2 ["id"] = 2
+bicicleta2 ["material"] = "legos"
+bicicleta2 ["ancho"] = 1.5
+bicicleta2 ["largo"] = 1.9
+bicicleta2 ["alto"] = 1.2
+bicicleta2 ["esFuncional"] = False
+bicicleta2 ["esEstable"] = True
+bicicleta2 ["esParecida"] = False
