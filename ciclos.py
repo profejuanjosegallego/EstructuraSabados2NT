5d3bd48f5d797d6b2614de233ed59e74e076b654
sara <75278809+Sarahoyos@users.noreply.github.com>
2026-02-14 14:33:15 -0500

    se crea ciclo for y while

---
+++ ciclos.py
@@ -0,0 +1,15 @@
+#ciclo while 
+# contador=1
+# while contador>10:
+#     print("hola")
+#     contador+=1
+# print("adios")
+     
+ 
+ 
+ 
+ #ciclo for 
+ 
+for contador in range(1,5):
+    print(contador)
+    
