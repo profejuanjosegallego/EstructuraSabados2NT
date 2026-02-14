a6fb41c62db46cf5fc06b15f20f5726aa361ded4
nicolas suarez <nicolassuarezre123@gmail.com>
2026-02-14 13:54:18 -0500

    feat se crea diccionario bicicleta

--- diccionario.py
+++ diccionario.py
@@ -15,3 +15,15 @@ estudiante["correo"]="correo@gmail.com"
 estudiante["semestre"]= 3
 estudiante["nombre"]="nicolas suarez"
 estudiante["esBequedo"]=True
+
+
+bicicleta={
+    "id": 1,
+    "materia": "plastilina",
+    "ancho": 1.5,
+    "largo": 2.0,
+    "alto": 1.0,
+    "esFuncional":False,
+    "esEstable":True,
+    "esParecido":True
+}
