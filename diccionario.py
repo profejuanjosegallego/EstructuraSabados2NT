10d6f9d0d16e2b8ae31b707f57c5c10d82a596d6
Santiago <kampuzano788@gmail.com>
2026-02-14 11:48:36 -0500

    feat se crea diccionario estudiante

---
+++ diccionario.py
@@ -0,0 +1,12 @@
+#Creando un diccionario
+
+estudiante = {}
+
+estudiante["id"] = 121914
+estudiante["documento"] = "12345678"
+estudiante["telefono"] = "555-1234" 
+estudiante["promedio"] = 8.5
+estudiante["correo"] = "correo@gmail.com"
+estudiante["semestre"] = 3
+estudiante["nombre"] = "Juan Perez"
+estudiante["esBecado"] = True
