7910904af5ddbc101bd50e6e7e9e1b706ae6f559
alexispernilla <alexispernilla@hotmail.com>
2026-02-14 11:48:51 -0500

    feat se crea diccinario estudiante

---
+++ diccionario.py
@@ -0,0 +1,12 @@
+#creando diccionario
+
+estudiante = {}
+
+#poblando un diccionario
+estudiante["id"] = 1222
+estudiante["nombre"] = "Juan"
+estudiante["documento"] = "323243322"
+estudiante["telefono"] = "3221232423"
+estudiante["promedio"] = 4.5
+estudiante["correo"] = "
+
