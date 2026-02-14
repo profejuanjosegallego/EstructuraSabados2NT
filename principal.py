36b61cb7fd8e55278f2143cea10d862833ab20b9
Midnight <hamiltonechavarria@gmail.com>
2026-02-14 11:49:25 -0500

    feat se crea diccionario estudiante

---
+++ principal.py
@@ -0,0 +1,8 @@
+idEstudiante=123456789
+documentoEstudiante="2322323"
+telefonoEstudiante="123456789"
+promedioEstudiante=3.5
+correoEstudiante="correo@ejemplo.com"
+semestreEstudiante=3
+nombreEstudiante="Hamilton"
+esBecado=True
