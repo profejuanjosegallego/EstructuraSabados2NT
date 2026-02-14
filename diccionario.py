c4fda68834f9946f08561fe62b770e214c9a9567
Carlos Yepes Aristizábal <125563400+Cyac221@users.noreply.github.com>
2026-02-14 11:48:39 -0500

    feat se crea diccionario estudiante

---
+++ diccionario.py
@@ -0,0 +1,13 @@
+#Creando un diccionario
+estudiante = {}
+
+#poblando un diccionario
+
+estudiante["id"] = 3636363
+estudiante["documento"] = "1000291895"
+estudiante["telefono"] = "3012367681"
+estudiante["promedio"] = 4.5
+estudiante["correo"] = "carlosyepes@example.com"
+estudiante["semestre"] = 3
+estudiante["nombre"] = "Carlos Yepes"
+estudiante["esBequedo"] = False
---
+++ principal.py
@@ -0,0 +1,8 @@
+idEstudiante = 3636363
+documentoEstudiante = "1000291895"
+telefonoEstudiante = "3012367681"
+promedioEstudiante = 4.5
+correoEstudiante = "carlosyepes@example.com"
+semestreEstudiante = 3
+nombreEstudiante = "Carlos Yepes"
+esBequedo = False
