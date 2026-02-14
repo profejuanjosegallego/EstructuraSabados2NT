80b6c785695fcd148bf369ab4b1bded016cc0922
Andres Henao <andreshenao4712@gmail.com>
2026-02-14 11:48:51 -0500

    feat se crea diccionario estudiante

---
+++ diccionario.py
@@ -0,0 +1,11 @@
+#Creando un diccionario
+
+estudiante={}
+estudiante["id"]=3636363
+estudiante["documento"]="399329"
+estudiante["telefono"]="123456789"
+estudiante["promedio"]=3.5
+estudiante["correo"]="correo@gmail.com"
+estudiante["semestre"]=3
+estudiante["nombre"]="Juan Perez"
+estudiante["esBequedo"]=True 
---
+++ principal.py
@@ -0,0 +1,8 @@
+idEstudiantes=3636363
+docucumentoEstudiante="399329"
+telefonoEstudiante="123456789"
+promedioEstudiante=3.5
+correoEstudiante="correo@gmail.com"
+semestreEstudiante=3
+nombreEstudiante="Juan Perez"
+esBequedo=True
