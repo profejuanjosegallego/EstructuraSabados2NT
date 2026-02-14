77adb4d047e0e5ae1bff03f726fb3d7f4e063574
Gildardo Quiceno <tatoqg83@gmail.com>
2026-02-14 14:00:02 -0500

    feat se crea diccionario bicicleta

--- diccionario.py
+++ diccionario.py
@@ -12,3 +12,14 @@ estudiante["correo"]="correo@gmail.com"
 estudiante["semestre"]=3
 estudiante["nombre"]="Gildardo Quiceno"
 estudiante["esBecado"]=True
+
+bicicleta={
+    "id": 123456,
+    "materialBicicleta": ("plastilina", "lego"),
+    "ancho": 1.5,
+    "largo": 2.5,
+    "alto": 1.0,
+    "funcional": True,
+    "estable": True,
+    "parecido": True
+}
--- principal.py
+++ principal.py
@@ -6,3 +6,14 @@ correoEstudiante = "correo@gmail.com"
 semestreEstudiante = 3
 nombreEstudiante = "Gildardo Quiceno"
 esBecado = True
+
+idBicicleta = 123456
+materialBicicleta = "plastilina", "lego",
+anchoBicicleta = 1.5
+largoBicicleta = 2.5
+altoBicicleta = 1.0
+funcional = True
+estable = True
+parecido = True
+
+
