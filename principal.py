ddfac059b652a9616beac2a2c9bc288aa2111c49
Mateo Montoya <montoyaramirez86@gmail.com>
2026-02-14 11:48:50 -0500

    feat se crea diccionario estudiante

---
+++ principal.py
@@ -0,0 +1,8 @@
+idEstudiante=1000000
+documentoEstudiante="1000001"
+telefonoEstudiante="3177622563"
+promedioEstudiante=3.5
+correoEstudiante="correo@gmail.com"
+semestreEstudiante=3
+nombresEstudiante="Monet Garniz"
+esBecado=True
