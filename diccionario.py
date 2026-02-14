eb26035408e640bd8f1dd4907437d37f1732b0e0
Gildardo Quiceno <tatoqg83@gmail.com>
2026-02-14 11:46:48 -0500

    Se crea diccionario Estudiante

---
+++ diccionario.py
@@ -0,0 +1,14 @@
+#Crando un diccionario
+
+estudiante={
+    "id": 363636,
+}
+#poblando un diccionario (llenar)
+estudiante["id"]=363636
+estudiante["documento"]="233232"
+estudiante["telefono"]="123456789"
+estudiante["promedio"]=3.5
+estudiante["correo"]="correo@gmail.com"
+estudiante["semestre"]=3
+estudiante["nombre"]="Gildardo Quiceno"
+estudiante["esBecado"]=True
---
+++ principal.py
@@ -0,0 +1,8 @@
+idEstudiante = 363636
+documentoEstudiante = "233232"
+telefonoEstudiante = "123456789"
+promedioEstudiante = 3.5
+correoEstudiante = "correo@gmail.com"
+semestreEstudiante = 3
+nombreEstudiante = "Gildardo Quiceno"
+esBecado = True
