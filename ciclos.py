448ec8eb001b6dc595727b0635f492380501f3a6
Andres Henao <andreshenao4712@gmail.com>
2026-02-14 14:33:54 -0500

    feat se creo cilo for

---
+++ ciclos.py
@@ -0,0 +1,9 @@
+#CICLO WHILE
+'''contador=0
+while 5>2:
+    print("Hola")  
+print("Adios")'''
+#CICLO FOR
+
+for _ in range(5):
+    print("hola")
--- diccionario.py
+++ diccionario.py
@@ -9,3 +9,12 @@ estudiante["correo"]="correo@gmail.com"
 estudiante["semestre"]=3
 estudiante["nombre"]="Juan Perez"
 estudiante["esBequedo"]=True 
+
+bicicleta={}
+bicicleta["id"]="Trek"
+bicicleta["material"]="Aluminio"
+bicicleta["largo"]="1.8m"
+bicicleta["alto"]="1.2m"
+bicicleta["esfuncional"]="si"
+bicicleta["esestable"]="si"
+bicicleta["esparecido"]="si"
