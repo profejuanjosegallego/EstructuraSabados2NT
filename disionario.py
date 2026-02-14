5a4f258590f6069487c3248c0714a4b6f8868b8e
Camilo <cavenaghi0812@gmail.com>
2026-02-14 11:49:08 -0500

    feat se crea disionario estudiante

---
+++ disionario.py
@@ -0,0 +1,12 @@
+#creando un disionario
+estudiante={
+    "id":3343
+}
+estudiante["id"]=2323
+Estudinte["documento"]="12"
+Estudiante
+Estudiante
+Estudiante
+Estudiante
+Estudiante
+estudiante["esBequedo"]=True
