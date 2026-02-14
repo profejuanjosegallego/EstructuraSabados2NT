dba5e48d6ade7c13c9605b698fcb6809ebf5ae23
yeral <yeralvalderrama2@gmail.com>
2026-02-14 14:07:05 -0500

    feat diccionario

---
+++ bicicleta.py
@@ -0,0 +1,11 @@
+bicicleta={}
+bicicleta["id"]
+bicicleta["material"]
+bicicleta["ancho"]
+bicicleta["largo"]
+bicicleta["alto"]
+bicicleta["esFincional"]
+bicicleta["esEstable"]
+bicicleta["esParecido"]
+
+print("hola")
---
+++ principal.py
@@ -0,0 +1,8 @@
+idEstudiante=363636
+documentoEstudiante= "232323"
+telefonoEstudiante= "3225962363"
+promedioEstudiante= 3.5
+correoElectronico="correo@gmail.com"
+semestreEstudiante= 3
+nombreEstudiante= "juan pablo"
+esBecado= True
