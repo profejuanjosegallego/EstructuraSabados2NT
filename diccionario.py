5843a09b68aa088935095f9eca5a462aca64c788
sara <75278809+Sarahoyos@users.noreply.github.com>
2026-02-14 11:48:49 -0500

    feat se crea diccionario estudiante

---
+++ diccionario.py
@@ -0,0 +1,25 @@
+# Creando un diccionario
+estudiante = {
+    "id":334556,
+    "documento":2424252,
+     "telefono":25433635242,
+     "promedio":3.5,
+     "correo":"correo@gmail.com",
+    "semestre":3.5,
+    "nombre":"Juancho panzo",
+    "esbequedo":True
+    
+    
+    
+}
+
+
+#Llenando un diccionario (lista)
+estudiante=["id"]=235342
+estudiante=["documento"]="235342"
+estudiante=["telefono"]="3534532632"
+estudiante=["promedio"]=3.5
+estudiante=["correo"]="correo@gmail.com"
+estudiante=["semestre"]=3
+estudiante=["nombre"]="Juancho panazo"
+estudiante=["esbequedo"]=True
---
+++ principal.py
@@ -0,0 +1,11 @@
+idEstudiante=2324252
+documentoEstudiante="2424252"
+telefonoEstudiante="25433635242"
+promedioEstudiante=3.5
+correoEstudiante="correo@gmail.com"
+semestreEstudiante=3
+nombreEstudiante="Franklin Race"
+esBequedo=True
+
+
+nombres=["laura","carlos","cris","tim"]
