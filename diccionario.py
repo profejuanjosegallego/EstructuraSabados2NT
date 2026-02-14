33cd766bd0b73b73e2ea3321d1ee205f6311040e
Sara <szuluaga@cesde.net>
2026-02-14 14:02:17 -0500

    featureszt

---
+++ diccionario.py
@@ -0,0 +1,21 @@
+#creando un diccionario
+#en la vida real los comentarios son para probar un codigo con para hacer apuntes
+
+estudiante={
+
+    "id":102010
+
+}
+
+#POBLANDO UN DICCIONARIO (LLENAR)
+
+Estudiante ["id"]=102010
+Estudiante ["documento"]="1020103091"
+Estudiante["telefono"]=3225183450
+Estudiante ["promedio"]=3.5
+Estudiante["correo"]="correo@gmail.com"
+Estudiante["semestre"]=3
+Estudiante ["nombre"]="juan gallego"
+esBloqueado["bloqueado"]=True
+
+
