780933ed5b8d2a99df961a8e464ddf73307a7151
Miguel Zapata <miguel.zapata@omc.com>
2026-02-14 14:02:43 -0500

    feat se crea diccionario bicicleta

---
+++ diccionario.py
@@ -0,0 +1,26 @@
+estudiante={}
+
+
+#poblando un diccionario
+
+estudiante["id"]=26472208
+estudiante["documento"]="1034990999"
+estudiante["telefono"]="3011907282"
+estudiante["promedio"]="4"
+estudiante["correo"]="miguel.zapata@omc.com"
+estudiante["nombre"]="miguel zapata"
+estudiante["semestre"]=3
+estudiante["Becado"]=True
+
+
+
+bicicleta={}
+
+bicicleta["id"]=12345678
+bicicleta["material"]="aluminio"
+bicicleta["ancho"]="1.5"
+bicicleta["alto"]="1.2"
+bicicleta["esFuncional"]=True
+bicicleta["esEstable"]=True
+bicicleta["esParecida"]=True
+
---
+++ principal.py
@@ -0,0 +1,17 @@
+idEstudiante=26472208
+documentoEstudiante="1034990999"
+telefonoEstudiante="3011907282"
+promedioEstudiante="4"
+correoEstudiante="miguel.zapata@omc.com"
+nombreEstudiante="miguel zapata"
+semestreEstudiante=3
+Becado=True
+
+
+idBicicleta=12345678
+materialBicicleta="aluminio"
+anchoBicicleta="1.5"
+altoBicicleta="1.2"
+esFuncional=True
+esEstable=True
+esParecida=True
