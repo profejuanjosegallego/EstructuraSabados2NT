f5cab1dee76f23da26088461cf778a1e7546ebc7
yeral lopez <yeralvalderrama2@gmail.com>
2026-02-14 14:05:18 -0500

    feat se crea diccionario

---
+++ diccionario.py
@@ -0,0 +1,12 @@
+#Diccionario Estudiantes
+
+estudiante={}
+
+#Poblando un diccionario
+
+estudiante["id"]=1231354
+estudiante["documento"]="1231567"
+estudiante["telefono"]="3000000000"
+estudiante["correo"]="correo@correo.com"
+estudiante["nombre"]="Santiago"
+estudiante["edad"]="26"
