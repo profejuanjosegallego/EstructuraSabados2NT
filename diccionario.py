eeb4fb61d2b4f2b3457782f67f5d4bbc78c59b02
Alejo <alejozapata432.com>
2026-02-14 11:48:59 -0500

    feat se crea diccionario estudiante

---
+++ diccionario.py
@@ -0,0 +1,17 @@
+#Creando un diccionario
+estudiante = {
+    id: 32
+}
+
+#poblando el diccionario
+estudiante ["id"] = 32
+estudiante ["documento"] = "12345678"
+estudiante ["telefono"] = "555-1234"
+estudiante ["promedio"] = 3.5
+estudiante ["correo"] = "correo@gmail.com"
+estudiante ["semestre"] = 3
+estudiante ["nombre"] = "Juan Gallego"
+estudiante ["esBecado"] = True
+
+
+
---
+++ principal.py
@@ -0,0 +1,8 @@
+idEstudiante = 32
+documentoEstudiante = "12345678" 
+telefonoEstudiante = "555-1234" 
+promedioEstudiante = 3.5
+correoEstudiante = "correo@gmail.com"
+semestreEstudiante = 3 
+nombreEstudiante = "Juan Gallego"
+esBecado =True
